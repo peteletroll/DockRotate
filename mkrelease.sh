bef85f349c3742d07500a372e627b408fa9bec57
Pietro Cagnoni <pietro.cagnoni@wki.it>
2018-02-01 17:02:35 +0100

    added mkrelease.sh

---
+++ mkrelease.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd `dirname $0` || exit 1
+
+# [assembly: AssemblyVersion ("1.3.1.1")]
+
+version=`sed -n 's/.*\<AssemblyVersion\>.*"\([^"]\+\)".*/\1/p' DockRotate/Properties/AssemblyInfo.cs`
+if [ "$version" = "" ]
+then
+	echo "$0: can't find version number" 1>&2
+	exit 1
+fi
+
+echo version $version
+
