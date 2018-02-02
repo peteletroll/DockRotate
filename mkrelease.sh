edbed323cb4c66256b0b9ee54b4772e669a5ee66
peteleroll <pietro@superbia>
2018-02-02 17:14:57 +0100

    ModuleManager included in release

--- mkrelease.sh
+++ mkrelease.sh
@@ -32,6 +32,9 @@ do
 	fi
 done
 
+rm -f Resources/ModuleManager.*.dll
+cp ~/KSP/KSP_linux/GameData/ModuleManager.*.dll Resources || exit 1
+
 echo generating package in $tmp
 
 cp $dll README.md LICENSE.md Resources/* $dir || exit 1
