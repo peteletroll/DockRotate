211541b728de07e3eaf45021428850db4804f19c
peteletroll <pietro.cagnoni@gmail.com>
2018-02-22 14:21:45 +0100

    error handling

--- downloads.pl
+++ downloads.pl
@@ -8,7 +8,10 @@ use JSON;
 
 my ($user, $repository) = ("peteletroll", "DockRotate");
 
-my $j = from_json(get("https://api.github.com/repos/$user/$repository/releases"));
+my $url = "https://api.github.com/repos/$user/$repository/releases";
+my $j = get($url)
+	or die "$0: can't get $url\n";
+$j = from_json($j);
 foreach my $r (@$j) {
 	foreach my $a (@{$r->{assets}}) {
 		printf "%6d %s\n", $a->{download_count}, $a->{name};
