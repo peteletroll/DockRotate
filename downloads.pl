6b42f941b98e7234dbca363895a4aabfd8fe1db7
peteletroll <pietro.cagnoni@gmail.com>
2018-02-23 09:15:32 +0100

    added total

--- downloads.pl
+++ downloads.pl
@@ -12,9 +12,13 @@ my $url = "https://api.github.com/repos/$user/$repository/releases";
 my $j = get($url)
 	or die "$0: can't get $url\n";
 $j = from_json($j);
+my $total = 0;
 foreach my $r (@$j) {
 	foreach my $a (@{$r->{assets}}) {
-		printf "%6d %s\n", $a->{download_count}, $a->{name};
+		my $c = $a->{download_count};
+		printf "%6d %s\n", $c, $a->{name};
+		$total += $c;
 	}
 }
+printf "%6d total\n", $total;
 
