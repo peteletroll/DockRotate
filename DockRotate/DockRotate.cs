8eeff29c359bed214af5368b56eef8ef7d7b898e
peteletroll <pietro@superbia>
2018-12-25 12:23:17 +0100

    preparing for release

--- DockRotate/DockRotate.cs
+++ DockRotate/DockRotate.cs
@@ -2212,7 +2212,7 @@ namespace DockRotate
 			lprint("  linY: " + _jdump(j.yMotion, j.yDrive, j.linearLimit, j.linearLimitSpring));
 			lprint("  linZ: " + _jdump(j.zMotion, j.zDrive, j.linearLimit, j.linearLimitSpring));
 
-			lprint("  proj: " + j.projectionMode + " ang=" + j.projectionAngle + " dst=" + j.projectionDistance); 
+			lprint("  proj: " + j.projectionMode + " ang=" + j.projectionAngle + " dst=" + j.projectionDistance);
 		}
 
 		private static string _jdump(ConfigurableJointMotion mot, JointDrive drv, SoftJointLimit lim, SoftJointLimitSpring spr)
