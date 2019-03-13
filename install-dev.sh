6fa2312c3269111b0c0f43aa0e1f912b4a0f2ceb
peteletroll <pietro.cagnoni@gmail.com>
2019-03-13 12:57:41 +0100

    added install-dev.sh

---
+++ install-dev.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+name=DockRotate
+ksphome=~/KSP/KSP_linux
+
+debug=0
+while getopts d opt
+do
+	case $opt in
+	d)
+		debug=1
+		;;
+	*)
+		exit 1
+		;;
+	esac
+done
+shift `expr $OPTIND - 1`
+
+tmp=`mktemp -d` || exit 1
+trap "rm -rf $tmp" EXIT
+
+zip="$tmp/dr.zip"
+./mkrelease.sh -z "$zip" || exit 1
+
+gamedata="$ksphome/GameData"
+if [ ! -d "$gamedata" ]
+then
+	echo "$0: $gamedata directory not found" 1>&2
+	exit 1
+fi
+
+(
+	cd "$tmp" &&
+	unzip "$zip" &&
+	cd GameData || exit 1
+
+	if [ $debug -ne 0 ]
+	then
+		echo
+		find . -name \*.debugdll -exec bash -c 'dll="{}"; mv -v "$dll" "${dll%.debugdll}.dll"' \;
+	fi
+
+	echo
+	rm -rfv "$gamedata/$name" || exit 1
+	echo
+	cp -rv "$name" "$gamedata" || exit 1
+
+	echo
+) || exit 1
+
+
