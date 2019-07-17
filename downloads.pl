f0bbd20d19502c5eefc18acf978ce59d3069fa1b
peteletroll <pietro.cagnoni@gmail.com>
2019-07-17 12:16:44 +0200

    handle multipage releases list

--- downloads.pl
+++ downloads.pl
@@ -3,15 +3,26 @@
 use strict;
 use warnings;
 
+use URI;
 use LWP::Simple;
 use JSON;
 
 my ($user, $repository) = ("peteletroll", "DockRotate");
 
-my $url = "https://api.github.com/repos/$user/$repository/releases";
-my $j = get($url)
-	or die "$0: can't get $url\n";
-$j = from_json($j);
+my $url = URI->new("https://api.github.com/repos/$user/$repository/releases");
+my $page = 1;
+my $j = [ ];
+for (;;) {
+	$url->query_form(page => $page);
+	# warn "GET $url\n";
+	my $jp = get($url)
+		or die "$0: can't get $url\n";
+	$jp = from_json($jp);
+	@$jp or last;
+	push @$j, @$jp;
+	$page++;
+}
+
 my $total = 0;
 foreach my $r (reverse @$j) {
 	foreach my $a (@{$r->{assets}}) {
