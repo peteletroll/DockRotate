72cc4f57ee9f88d78114dd60daf376f968ff909b
peteletroll <>
2020-05-22 15:10:23 +0200

    splitted old DockRotate.cs

---
+++ DockRotate/ModuleDockRotate.cs
@@ -0,0 +1,220 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using KSP.Localization;
+using CompoundParts;
+
+namespace DockRotate
+{
+	public class ModuleDockRotate: ModuleBaseRotate
+	{
+		/*
+
+			docking node states:
+
+			* PreAttached
+			* Docked (docker/same vessel/dockee) - (docker) and (same vessel) are coupled with (dockee)
+			* Ready
+			* Disengage
+			* Acquire
+			* Acquire (dockee)
+
+		*/
+
+		private ModuleDockingNode dockingNode;
+
+		protected override void fillInfo()
+		{
+			storedModuleDisplayName = Localizer.Format("#DCKROT_port_displayname");
+			storedInfo = Localizer.Format("#DCKROT_port_info");
+		}
+
+		protected override AttachNode findMovingNodeInEditor(out Part otherPart, bool verbose)
+		{
+			otherPart = null;
+			if (!dockingNode || dockingNode.referenceNode == null)
+				return null;
+			if (verbose)
+				log(desc(), ".findMovingNodeInEditor(): referenceNode = " + dockingNode.referenceNode.desc());
+			AttachNode otherNode = dockingNode.referenceNode.findConnectedNode(verboseEvents);
+			if (verbose)
+				log(desc(), ".findMovingNodeInEditor(): otherNode = " + otherNode.desc());
+			if (otherNode == null)
+				return null;
+			otherPart = otherNode.owner;
+			if (verbose)
+				log(desc(), ".findMovingNodeInEditor(): otherPart = " + otherPart.desc());
+			if (!otherPart)
+				return null;
+			ModuleDockingNode otherDockingNode = otherPart.FindModuleImplementing<ModuleDockingNode>();
+			if (verbose)
+				log(desc(), ".findMovingNodeInEditor(): otherDockingNode = "
+					+ (otherDockingNode ? otherDockingNode.part.desc() : "null"));
+			if (!otherDockingNode)
+				return null;
+			if (verbose)
+				log(desc(), ".findMovingNodeInEditor(): otherDockingNode.referenceNode = "
+					+ otherDockingNode.referenceNode.desc());
+			if (otherDockingNode.referenceNode == null)
+				return null;
+			if (!otherDockingNode.matchType(dockingNode)) {
+				if (verbose)
+					log(desc(), ".findMovingNodeInEditor(): mismatched node types "
+						+ dockingNode.nodeType + " != " + otherDockingNode.nodeType);
+				return null;
+			}
+			if (verbose)
+				log(desc(), ".findMovingNodeInEditor(): node test is "
+					+ (otherDockingNode.referenceNode.FindOpposingNode() == dockingNode.referenceNode));
+
+			return dockingNode.referenceNode;
+		}
+
+		protected override bool setupLocalAxis(StartState state)
+		{
+			dockingNode = part.FindModuleImplementing<ModuleDockingNode>();
+
+			if (!dockingNode) {
+				log(desc(), ".setupLocalAxis(" + state + "): no docking node");
+				return false;
+			}
+
+			partNodePos = Vector3.zero.Tp(dockingNode.T(), part.T());
+			partNodeAxis = Vector3.forward.Td(dockingNode.T(), part.T());
+			if (verboseEvents)
+				log(desc(), ".setupLocalAxis(" + state + ") done: "
+					+ partNodeAxis + "@" + partNodePos);
+			return true;
+		}
+
+		protected override PartJoint findMovingJoint(bool verbose)
+		{
+			if (!dockingNode || !dockingNode.part) {
+				if (verbose)
+					log(desc(), ".findMovingJoint(): no docking node");
+				return null;
+			}
+
+			ModuleDockingNode other = dockingNode.otherNode;
+			if (other) {
+				if (verbose)
+					log(desc(), ".findMovingJoint(): other is " + other.part.desc());
+			} else if (dockingNode.dockedPartUId > 0) {
+				other = dockingNode.FindOtherNode();
+				if (verbose && other)
+					log(desc(), ".findMovingJoint(): other found " + other.part.desc());
+			}
+
+			if (!other || !other.part) {
+				if (verbose)
+					log(desc(), ".findMovingJoint(): no other, id = " + dockingNode.dockedPartUId);
+				return null;
+			}
+
+			if (!dockingNode.matchType(other)) {
+				if (verbose)
+					log(desc(), ".findMovingJoint(): mismatched node types");
+				return null;
+			}
+
+			if (verbose && dockingNode.state != "PreAttached"
+				&& !dockingNode.state.StartsWith("Docked", StringComparison.InvariantCulture))
+				log(desc(), ".findMovingJoint(): unconnected state \"" + dockingNode.state + "\"");
+
+			ModuleBaseRotate otherModule = other.part.FindModuleImplementing<ModuleBaseRotate>();
+			if (otherModule) {
+				if (!smartAutoStruts && otherModule.smartAutoStruts) {
+					smartAutoStruts = true;
+					log(desc(), ".findMovingJoint(): smartAutoStruts activated by " + otherModule.desc());
+				}
+			}
+
+			PartJoint ret = dockingNode.sameVesselDockJoint;
+			if (ret && ret.Target == other.part) {
+				if (verbose)
+					log(desc(), ".findMovingJoint(): to same vessel " + ret.desc());
+				return ret;
+			}
+
+			ret = other.sameVesselDockJoint;
+			if (ret && ret.Target == dockingNode.part) {
+				if (verbose)
+					log(desc(), ".findMovingJoint(): from same vessel " + ret.desc());
+				return ret;
+			}
+
+			if (dockingNode.part.parent == other.part) {
+				ret = dockingNode.part.attachJoint;
+				if (verbose)
+					log(desc(), ".findMovingJoint(): to parent " + ret.desc());
+				return ret;
+			}
+
+			for (int i = 0; i < dockingNode.part.children.Count; i++) {
+				Part child = dockingNode.part.children[i];
+				if (child == other.part) {
+					ret = child.attachJoint;
+					if (verbose)
+						log(desc(), ".findMovingJoint(): to child " + ret.desc());
+					return ret;
+				}
+			}
+
+			if (verbose)
+				log(desc(), ".findMovingJoint(): nothing");
+			return null;
+		}
+
+		protected override void doSetup()
+		{
+			base.doSetup();
+
+			if (hasJointMotion && jointMotion.joint.Host == part && !frozenFlag) {
+				float snap = autoSnapStep();
+				if (verboseEvents)
+					log(desc(), ".autoSnapStep() = " + snap);
+				ModuleDockRotate other = jointMotion.joint.Target.FindModuleImplementing<ModuleDockRotate>();
+				if (other) {
+					float otherSnap = other.autoSnapStep();
+					if (verboseEvents)
+						log(other.desc(), ".autoSnapStep() = " + otherSnap);
+					if (otherSnap > 0f && (snap.isZero() || otherSnap < snap))
+						snap = otherSnap;
+				}
+				if (!snap.isZero()) {
+					if (verboseEvents)
+						log(jointMotion.desc(), ": autosnap at " + snap);
+					enqueueFrozenRotation(jointMotion.angleToSnap(snap), 5f);
+				}
+			}
+		}
+
+		private float autoSnapStep()
+		{
+			if (!hasJointMotion || !dockingNode)
+				return 0f;
+
+			float step = 0f;
+			string source = "no source";
+			if (dockingNode.snapRotation && dockingNode.snapOffset > 0.01f) {
+				step = dockingNode.snapOffset;
+				source = "snapOffset";
+			} else if (autoSnap && rotationEnabled && rotationStep > 0.01f) {
+				step = rotationStep;
+				source = "rotationStep";
+			}
+			if (verboseEvents)
+				log(desc(), ".autoSnapStep() = " + step + " from " + source);
+			if (step >= 360f)
+				step = 0f;
+			return step;
+		}
+
+		public override string descPrefix()
+		{
+			return "MDR";
+		}
+	}
+}
+
---
+++ DockRotate/ModuleNodeRotate.cs
@@ -0,0 +1,155 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using KSP.Localization;
+using CompoundParts;
+
+namespace DockRotate
+{
+	public class ModuleNodeRotate: ModuleBaseRotate
+	{
+		[KSPField(isPersistant = true)]
+		public string rotatingNodeName = "";
+
+		[KSPField(isPersistant = true)]
+		public uint otherPartFlightID = 0;
+
+		public AttachNode rotatingNode;
+
+		private bool isSrfAttach()
+		{
+			return rotatingNodeName == "srfAttach";
+		}
+
+		protected override void fillInfo()
+		{
+			storedModuleDisplayName = Localizer.Format("#DCKROT_node_displayname");
+			storedInfo = Localizer.Format("#DCKROT_node_info", rotatingNodeName);
+		}
+
+		protected override void doSetup()
+		{
+			base.doSetup();
+			// TODO: change groupDisplayName to "NodeRotate <node name>"
+		}
+
+		protected override AttachNode findMovingNodeInEditor(out Part otherPart, bool verbose)
+		{
+			otherPart = null;
+			if (rotatingNode == null)
+				return null;
+			if (verbose)
+				log(desc(), ".findMovingNodeInEditor(): rotatingNode = " + rotatingNode.desc());
+			otherPart = rotatingNode.attachedPart;
+			if (verbose)
+				log(desc(), ".findMovingNodeInEditor(): otherPart = " + otherPart.desc());
+			if (!otherPart)
+				return null;
+			if (verbose)
+				log(desc(), ".findMovingNodeInEditor(): attachedPart = " + rotatingNode.attachedPart.desc());
+			return rotatingNode;
+		}
+
+		protected override bool setupLocalAxis(StartState state)
+		{
+			rotatingNode = part.FindAttachNode(rotatingNodeName);
+			if (rotatingNode == null && isSrfAttach())
+				rotatingNode = part.srfAttachNode;
+
+			if (rotatingNode == null) {
+				log(desc(), ".setupLocalAxis(" + state + "): "
+					+ "no node \"" + rotatingNodeName + "\"");
+
+				AttachNode[] nodes = part.allAttachNodes();
+				for (int i = 0; i < nodes.Length; i++)
+					log(desc(), ": node[" + i + "] = " + nodes[i].desc());
+				return false;
+			}
+
+			partNodePos = rotatingNode.position;
+			partNodeAxis = rotatingNode.orientation;
+			if (verboseEvents)
+				log(desc(), ".setupLocalAxis(" + state + ") done: "
+					+ partNodeAxis + "@" + partNodePos);
+			return true;
+		}
+
+		protected override PartJoint findMovingJoint(bool verbose)
+		{
+			uint prevOtherPartFlightID = otherPartFlightID;
+			otherPartFlightID = 0;
+
+			if (rotatingNode == null || !rotatingNode.owner) {
+				if (verbose)
+					log(desc(), ".findMovingJoint(): no node");
+				return null;
+			}
+
+			if (part.FindModuleImplementing<ModuleDockRotate>()) {
+				log(desc(), ".findMovingJoint(): has DockRotate, NodeRotate disabled");
+				return null;
+			}
+
+			Part owner = rotatingNode.owner;
+			Part other = rotatingNode.attachedPart;
+			if (!other) {
+				if (verbose)
+					log(desc(), ".findMovingJoint(" + rotatingNode.id + "): attachedPart is null, try by id = "
+						+ prevOtherPartFlightID);
+				other = findOtherById(prevOtherPartFlightID);
+			}
+			if (!other) {
+				if (verbose)
+					log(desc(), ".findMovingJoint(" + rotatingNode.id + "): no attachedPart");
+				return null;
+			}
+			if (verbose && other.flightID != prevOtherPartFlightID)
+				log(desc(), ".findMovingJoint(" + rotatingNode.id + "): otherFlightID "
+					+ prevOtherPartFlightID + " -> " + other.flightID);
+			if (verbose)
+				log(desc(), ".findMovingJoint(" + rotatingNode.id + "): attachedPart is " + other.desc());
+			other.forcePhysics();
+			JointLockStateProxy.register(other, this);
+
+			if (owner.parent == other) {
+				PartJoint ret = owner.attachJoint;
+				if (verbose)
+					log(desc(), ".findMovingJoint(" + rotatingNode.id + "): child " + ret.desc());
+				otherPartFlightID = other.flightID;
+				return ret;
+			}
+
+			if (other.parent == owner) {
+				PartJoint ret = other.attachJoint;
+				if (verbose)
+					log(desc(), ".findMovingJoint(" + rotatingNode.id + "): parent " + ret.desc());
+				otherPartFlightID = other.flightID;
+				return ret;
+			}
+
+			if (verbose)
+				log(desc(), ".findMovingJoint(" + rotatingNode.id + "): nothing");
+			return null;
+		}
+
+		private Part findOtherById(uint id)
+		{
+			if (id == 0)
+				return null;
+			if (part.parent && part.parent.flightID == id)
+				return part.parent;
+			if (part.children != null)
+				for (int i = 0; i < part.children.Count; i++)
+					if (part.children[i] && part.children[i].flightID == id)
+						return part.children[i];
+			return null;
+		}
+
+		public override string descPrefix()
+		{
+			return "MNR";
+		}
+	}
+}
+
