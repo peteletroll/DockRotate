8064e877021b304e40a8ede1df8294e74124312a
peteletroll <>
2025-09-15 17:24:39 +0200

    LWP -> HTTP::Tiny

--- downloads.pl
+++ downloads.pl
@@ -4,20 +4,21 @@ use strict;
 use warnings;
 
 use URI;
-use LWP::Simple;
+use HTTP::Tiny;
 use JSON;
 
 my ($user, $repository) = ("peteletroll", "DockRotate");
 
+my $ua = HTTP::Tiny->new();
 my $url = URI->new("https://api.github.com/repos/$user/$repository/releases");
 my $page = 1;
 my $j = [ ];
 for (;;) {
 	$url->query_form(page => $page);
 	# warn "GET $url\n";
-	my $jp = get($url)
-		or die "$0: can't get $url\n";
-	$jp = from_json($jp);
+	my $res = $ua->get($url);
+	$res->{success} or die "$0: can't get $url: $res->{status} $res->{reason}\n";
+	my $jp = from_json($res->{content});
 	@$jp or last;
 	push @$j, @$jp;
 	$page++;
